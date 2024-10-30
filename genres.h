1f14c6efd3200660a077ea33f16a336bb4e25636
Kevin Nielsen <kan@fairytalesgroup.com>
2024-10-30 10:30:26 +0100

    flyt genres og themes til seperate filer

---
+++ genres.h
@@ -0,0 +1,29 @@
+#ifndef GENRES_H
+#define GENRES_H
+
+typedef enum Genres {
+    ACTION,
+    ADVENTURE_GENRE,
+    ANIMATION,
+    BIOGRAPHY,
+    COMEDY_GENRE,
+    CRIME,
+    DRAMA,
+    FAMILY_GENRE,
+    FANTASY,
+    FILM_NOIR,
+    HISTORY,
+    HORROR,
+    MUSIC,
+    MUSICAL,
+    MYSTERY,
+    ROMANCE,
+    SCI_FI,
+    SPORT,
+    THRILLER,
+    WAR,
+    WESTERN,
+    RETRO,
+} Genres;
+
+#endif // GENRES_H
