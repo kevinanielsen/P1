19bca7b5601b87e374b61aa84282b4ede2795bcb
Kevin Nielsen <kan@fairytalesgroup.com>
2024-10-31 09:53:09 +0100

    updated max chars

--- dataLoad.c
+++ dataLoad.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 #include "movie.h"
-
-
 #define MAXCHAR 500
-
 
 int dataLoad(void) {
     FILE *fp; // Initializing file pointer
-
     fp = fopen("IMDBTop250Movies.csv", "r"); // r - read mode
     if (fp == NULL) { 
         printf("Error opening file.\n");
         return 1;
     }
-
     Movie variable[250]; // Åbner for 250 tomme "pladser" i structuren movieVariables
     int counter = 0; // Bruges som en counter til hver linje gennemgået.
     char line[MAXCHAR]; // Et array med MAXCHAR pladser.
     
-    while (fgets(line, MAXCHAR, fp) != NULL && counter < 251) {  // While loop som anvender funktionen fgets. Den skal bruge et sted at opbevare dataen, maks antal characters den skal læse, og fra hvilken fil. Hvis det lykkedes opbevares dataen i det første parameter. Stopper hvis filen er tom, og derfor er NULL.
-        sscanf(line, "%d; %69[^;]; %d; %69[^;]; %69[^\n]", 
-               &variable[counter].rank, variable[counter].title, 
-               &variable[counter].sadness, variable[counter].genre, 
-               variable[counter].theme);
-        // De indsatte værdier er det maksimale antal characters. Hvert placeholder stemmer overens med CSV filens rækkefølge.
+    while (fgets(line, MAXCHAR, fp) != NULL && counter < 250) {  // While loop som anvender funktionen fgets. Den skal bruge et sted at opbevare dataen, maks antal characters den skal læse, og fra hvilken fil. Hvis det lykkedes opbevares dataen i det første parameter. Stopper hvis filen er tom, og derfor er NULL.
+        if (sscanf(line, "%d; %100[^;]; %d; %100[^;]; %100[^\n]", 
+                   &variable[counter].rank, variable[counter].title, 
+                   &variable[counter].sadness, variable[counter].genre, 
+                   variable[counter].theme) != 5) {
+            printf("Error parsing line: %s\n", line);
+            continue;
+        }
         counter++;
     }
     fclose(fp);
