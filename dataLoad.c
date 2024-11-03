eb27c635eb843c13f293c5dbf250e0498ccad823
Mathias0810 <barbarflamee@gmail.com>
2024-11-03 21:10:02 +0100

    Søgealgoritme1

--- dataLoad.c
+++ dataLoad.c
@@ -4,18 +4,19 @@
 #include "movie.h"
 #define MAXCHAR 500
 
-Movie *dataLoad(void) {
+int dataLoad(void) {
     FILE *fp; // Initializing file pointer
     fp = fopen("IMDBTop250Movies.csv", "r"); // r - read mode
     if (fp == NULL) { 
         printf("Error opening file.\n");
-        return NULL;
+        return 1;
     }
  // Åbner for 250 tomme "pladser" i structuren movieVariables
     int counter = 0; // Bruges som en counter til hver linje gennemgået.
     char line[MAXCHAR]; // Et array med MAXCHAR pladser.
     
     fgets(line, MAXCHAR, fp);
+ 
 
     while (fgets(line, MAXCHAR, fp) != NULL && counter < 250) {
           // While loop som anvender funktionen fgets. Den skal bruge et sted at opbevare dataen, maks antal characters den skal læse, og fra hvilken fil. Hvis det lykkedes opbevares dataen i det første parameter. Stopper hvis filen er tom, og derfor er NULL.
@@ -29,17 +30,5 @@ Movie *dataLoad(void) {
         counter++;
     }
     fclose(fp);
-
-    for (int i = 0; i < 251; i++) {
-        char genres[5] = {};
-        sscanf(movies[i].genre, "%100[^,]", &genres[0]);
-        for (int j = 0; j < 5; j++) {
-            if (genres[j] == '\0') {
-                break;
-            }
-            printf("Genre: %s\n", genres[j]);
-        }
-    }
-
-    return movies;
+    return 0;
 }
--- dataLoad.h
+++ dataLoad.h
@@ -1,10 +1,8 @@
 #ifndef dataLoad_H
 #define dataLoad_H
 
-#include "movie.h"
-
 // Function prototypes, macros, etc.
-Movie* dataLoad(void);
+int dataLoad(void);
 
 
 #endif //dataLoad_H
