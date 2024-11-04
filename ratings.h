abb55e78efb486d0f96b89de20bd0d66908a5474
Tobias Hannibal Nielsen <tobiashannibalnielsen@gmail.com>
2024-11-04 12:32:26 +0100

    Added variable  'ratedThemes' //struct

--- ratings.h
+++ ratings.h
@@ -1,10 +1,18 @@
 #ifndef ratings_H
 #define ratings_H
+#include "themes.h"
 
 typedef struct Ratings{
     const char *title; 
     int rating;        
 } Ratings;
+
+typedef struct RatedTheme {
+  Themes theme;
+  int Points;
+} RatedTheme;
+
+RatedTheme RatedThemes[25];
 
 // Function prototypes, macros, etc.
 Ratings *rate10Movies();
