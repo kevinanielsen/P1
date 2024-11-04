d1b56ed7fa2478c73e424e6df23832294d029c77
Kevin Nielsen <kan@fairytalesgroup.com>
2024-11-04 10:55:48 +0100

    output via paramter

--- search.c
+++ search.c
@@ -2,9 +2,8 @@
 #include <string.h>
 #include "movie.h"
 
-void searchGenre(Movie movies[], int movieCount, char *keyword) {
+void searchGenre(Movie movies[], int movieCount, char *keyword, int *searchedGenreIndex) {
     int foundCount = 0;
-    int searchedGenreIndex[250];
     int j = 0;
     for (int i = 0; i < movieCount; i++) {
         if (strstr(movies[i].genre, keyword) != NULL) {
@@ -18,13 +17,12 @@ void searchGenre(Movie movies[], int movieCount, char *keyword) {
     }
 }
 
-void searchTheme(Movie movies[], int movieCount, char *keyword) {
+void searchTheme(Movie movies[], int movieCount, char *keyword, int *resultIndexes) {
     int foundCount = 0;
-    int searchedThemeIndex[250];
     int j = 0;
     for (int i = 0; i < movieCount; i++) {
         if (strstr(movies[i].theme, keyword) != NULL) {
-            searchedThemeIndex[j] = i;
+            resultIndexes[j] = i;
             foundCount++;
             j++;
         }
