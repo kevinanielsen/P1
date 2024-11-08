ed6a76251844797ec1254a064082be3cbf86c11f
Elias Razi <Kontakt.eliasrazi@gmail.com>
2024-11-08 08:40:03 +0100

    har lavet grading af film baseret på themes

---
+++ grading.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "themes.h"
+#include "ratings.c"
+#include "movie.h"
+
+void grading (RatedTheme ratedThemes[], Movie *movies){
+    
+
+}
+
+    // const char *moviesThemes[10][5] = {
+    //     {"Hope", "Friendship", "Redemption", "Isolation", "Courage"},
+    //     {"Good vs Evil", "Power and Corruption", "Fear", "Justice", "Sacrifice"},
+    //     {"Power and Corruption", "Identity", "Betrayal", "Dreams and Aspirations", "Freedom"},
+    //     {"Family", "Identity", "Love", "Dreams and Aspirations", "Coming of Age"},
+    //     {"Dreams and Aspirations", "Adventure", "Coming of Age", "Identity", "Family"},
+    //     {"Revenge", "Courage", "Power and Corruption", "Sacrifice", "Survival"},
+    //     {"Love", "Sacrifice", "War and Peace", "Friendship", "Loss"},
+    //     {"Technology and Humanity", "Love", "Hope", "Adventure", "Survival"},
+    //     {"Identity", "Freedom", "Mental Health", "Good vs Evil"},
+    //     {"Technology and Humanity", "Isolation", "Courage", "Adventure", "Comedy"}
+    // };
