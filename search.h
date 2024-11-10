94449472f856b6d115bf5f188212ca1f6b8f0908
Kevin Nielsen <kevinanielsen@outlook.com>
2024-11-10 22:23:52 +0100

    add function for getting movie based on title

--- search.h
+++ search.h
@@ -5,4 +5,5 @@
 
 void searchGenre(Movie movies[], int movieCount, char *keyword, int *resultIndexes);
 void searchTheme(Movie movies[], int movieCount, const char *keyword, int *resultIndexes);
+int getMovieByTitle(Movie movies[], int movieCount, const char *title);
 #endif
