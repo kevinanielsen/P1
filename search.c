cc604e81b1d4ec01fb4f04de4222ae836b377d2e
Kevin Nielsen <kevinanielsen@outlook.com>
2024-11-11 10:29:10 +0100

    format

--- search.c
+++ search.c
@@ -1,37 +1,38 @@
+#include "movie.h"
 #include <stdio.h>
 #include <string.h>
-#include "movie.h"
 
-void searchGenre(Movie movies[], int movieCount, char *keyword, int *searchedGenreIndex) {
-    int foundCount = 0;
-    int j = 0;
-    for (int i = 0; i < movieCount; i++) {
-        if (strstr(movies[i].genre, keyword) != NULL) {
-            searchedGenreIndex[j] = i; 
-            foundCount++;
-            j++;
-        }
+void searchGenre(Movie movies[], int movieCount, char *keyword,
+                 int *searchedGenreIndex) {
+  int foundCount = 0;
+  int j = 0;
+  for (int i = 0; i < movieCount; i++) {
+    if (strstr(movies[i].genre, keyword) != NULL) {
+      searchedGenreIndex[j] = i;
+      foundCount++;
+      j++;
     }
-    if (foundCount == 0) {
-        printf("No movies found with genre containing '%s'.\n", keyword);
-    }
+  }
+  if (foundCount == 0) {
+    printf("No movies found with genre containing '%s'.\n", keyword);
+  }
 }
 
-void searchTheme(Movie movies[], int movieCount, char *keyword, int *resultIndexes) {
-    int foundCount = 0;
-    int j = 0;
-    for (int i = 0; i < movieCount; i++) {
-        if (strstr(movies[i].theme, keyword) != NULL) {
-            resultIndexes[j] = i;
-            foundCount++;
-            j++;
-        }
+void searchTheme(Movie movies[], int movieCount, char *keyword,
+                 int *resultIndexes) {
+  int foundCount = 0;
+  int j = 0;
+  for (int i = 0; i < movieCount; i++) {
+    if (strstr(movies[i].theme, keyword) != NULL) {
+      resultIndexes[j] = i;
+      foundCount++;
+      j++;
     }
-    if (foundCount == 0) {
-        printf("No movies found with genre containing '%s'.\n", keyword);
-    }
+  }
+  if (foundCount == 0) {
+    printf("No movies found with genre containing '%s'.\n", keyword);
+  }
 }
-
 
 int getMovieByTitle(Movie movies[], int movieCount, char *title) {
   for (int i = 0; i < movieCount; i++) {
@@ -43,9 +44,10 @@ int getMovieByTitle(Movie movies[], int movieCount, char *title) {
     if (start != NULL && end != NULL && start != end) {
       // Calculate the length of the substring between the apostrophes
       size_t length = end - start - 1;
-      
+
       // Compare the substring with `movies[i].title`
-      if (strncmp(movies[i].title, start + 1, length) == 0 && movies[i].title[length] == '\0') {
+      if (strncmp(movies[i].title, start + 1, length) == 0 &&
+          movies[i].title[length] == '\0') {
         return i;
       }
     }
