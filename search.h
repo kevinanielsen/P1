bd240fd411ef3b42af8f2715173e047f7ddd8d36
Kevin Nielsen <kevinanielsen@outlook.com>
2024-12-12 13:54:27 +0100

    mae searchGenre function return a foundCount

--- search.h
+++ search.h
@@ -2,7 +2,7 @@
 #define search_H
 #include "movie.h"
 
-void searchGenre(Movie movies[], int movieCount, char *keyword, int *resultIndexes);
+int searchGenre(Movie movies[], int movieCount, char *keyword, int *resultIndexes);
 void searchTheme(Movie movies[], int movieCount, const char *keyword, int *resultIndexes);
 int getMovieByTitle(Movie movies[], int movieCount, const char *title);
 #endif
